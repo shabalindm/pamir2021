acfc0d6c1de6bee502f7e45029c7dc63411d0a6f
d.shabalin <d.shabalin@ticketland.ru>
2022-01-04 23:13:49 +0300

    --

--- photo/script/8.sh
+++ photo/script/8.sh
@@ -1,6 +1,6 @@
 cd ../..
 rm photo/8/*
-for file in DSC01332 DSC01369 DSC01377 DSC01390 DSC01437 DSC01467
+for file in    DSC01390  DSC01467
 do
  convert "photo_original/8/$file.JPG" -resize $p "photo/8/$file.JPG"
 done
