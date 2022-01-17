5a6f16731a41d402e7ec620844cbcc410b5bb240
d.shabalin <d.shabalin@ticketland.ru>
2022-01-17 21:25:17 +0300

    --

--- photo/script/resize_lisa.sh
+++ photo/script/resize_lisa.sh
@@ -3,10 +3,10 @@ do
   for file in $directory/*.JPG
   do
     mysize=$(find "$file" -printf "%s")
-  if [ $mysize -gt 5000000 ] #[$mysize -gt 10000000]
+  if [ $mysize -gt 3000000 ] #[$mysize -gt 10000000]
   then
       echo $file
-      convert $file -resize 30% $file
+      convert $file -resize 35% $file
   fi
 
   done
