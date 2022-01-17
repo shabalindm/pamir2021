3dd2c6da22463981ce607d609967500994afcea1
d.shabalin <d.shabalin@ticketland.ru>
2022-01-17 22:07:03 +0300

    --

--- photo/script/resize_lisa.sh
+++ photo/script/resize_lisa.sh
@@ -3,10 +3,10 @@ do
   for file in $directory/*.JPG
   do
     mysize=$(find "$file" -printf "%s")
-  if [ $mysize -gt 1500000 ] #[$mysize -gt 10000000]
+  if [ $mysize -gt 1000000 ] #[$mysize -gt 10000000]
   then
       echo $file
-      convert $file -resize 50% $file
+      convert $file -resize 60% $file
   fi
 
   done
--- photo/script/rm.java
+++ photo/script/rm.java
@@ -7,23 +7,21 @@ import java.nio.file.Path;
 
 public class rm {
     public static void main(String[] args) throws IOException {
-        File dir = new File("/git/pamir2021/photo/script");
+        File dir = new File("/git/pamir2021/photo");
         String[] list = dir.list();
         String indexHtml = Files.readString(Path.of("/git/pamir2021/index.html"));
-        for (String file : list) {
-            if(file.matches("\\d+\\.sh")){
-                String dayNum = file.replace(".sh", "");
-                File sh = new File(dir, file);
-                String shContent = Files.readString(sh.toPath());
-                String[] split = shContent.split("\\s+");
-                for (String w : split) {
-                    if(w.startsWith("DSC")){
-                        if(!indexHtml.contains(dayNum + "/" + w)){
-                            shContent = shContent.replace(w, "");
-                        }
+        for (String  day: list) {
+            if(day.matches("\\d+")){
+                File dayDir = new File(dir, day);
+                String[] photoFiles = dayDir.list();
+                for (String photoFile : photoFiles) {
+                    String name = "photo/" + day + "/" + photoFile;
+                    if(!indexHtml.contains(name)){
+                        File file = new File(dayDir, photoFile);
+                        file.delete();
                     }
                 }
-                Files.write(sh.toPath(), shContent.getBytes(StandardCharsets.UTF_8));
+
             }
 
         }
